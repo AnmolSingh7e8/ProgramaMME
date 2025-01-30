12c59ac97cb6295eedd08d1c1f79a291da81b574
anmolpreet.singh.7e8 <anmolpreet.singh.7e8@itb.cat>
2025-01-30 10:10:20 +0100

    Act 1

---
+++ src/src/Act1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Act1 {
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int numVegades = scan.nextInt();
+    for(int i = 0; i < numVegades; i++){
+        System.out.println("Hola mundo.");
+
+    }
+    }
+}
